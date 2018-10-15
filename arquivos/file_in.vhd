2e46da03832767b62e096f2d557bf8c22f80f5ab
Renan Augusto Starke <xtarke@gmail.com>
2018-10-15 10:37:23 -0300

    Add files examples

---
+++ arquivos/file_in.vhd
@@ -0,0 +1,22 @@
+entity file_in is
+  port (dado : out integer range 0 to 100);
+end file_in;
+
+architecture teste of file_in is
+begin
+
+  le: process
+    type arqv_int is file of integer;
+    file arquivo_rd: arqv_int is in "dado_a.dat";
+    variable dado_v: integer;
+
+  begin
+    while not endfile (arquivo_rd) loop
+      read(arquivo_rd, dado_v);
+      dado <= dado_v;
+      wait for 10 ns;
+    end loop;
+
+    wait;
+  end process le;
+end teste;
