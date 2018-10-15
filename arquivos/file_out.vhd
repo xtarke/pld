d1ac2ad88059c4f0fc62ba82fb722b9425333dbb
Renan Augusto Starke <xtarke@gmail.com>
2018-10-15 11:01:18 -0300

    Update file_out.vhd

--- arquivos/file_out.vhd
+++ arquivos/file_out.vhd
@@ -7,9 +7,15 @@ architecture RTL of file_out is
 begin
 
 	-- processo de escrita
-	escreve: process
+	escreve: process	
+		
 		type arqv_int is file of integer;
-		file arquivo_wr : arqv_int is out "dado_a.dat";
+		-- VHDL '87 
+		-- file arquivo_wr : arqv_int is out "dado_a.dat";
+		
+		-- VHDL '93
+		file arquivo_wr  : arqv_int open write_mode is "dado_a.dat";	
+		
 	begin   
 	    for dado in 48 to 57 loop
 			write (arquivo_wr, dado);
