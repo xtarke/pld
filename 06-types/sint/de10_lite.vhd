eb2f460d61d610a8a722ac8bf344e2cd0cfec202
Renan Augusto Starke <xtarke@gmail.com>
2020-04-29 14:38:37 -0300

    Added symthesis project.

---
+++ 06-types/sint/de10_lite.vhd
@@ -0,0 +1,102 @@
+-------------------------------------------------------------------
+-- Name        : de0_lite.vhd
+-- Author      : 
+-- Version     : 0.1
+-- Copyright   : Departamento de Eletrônica, Florianópolis, IFSC
+-- Description : Projeto base DE10-Lite
+-------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity de10_lite is 
+	port (
+		---------- CLOCK ----------
+		ADC_CLK_10:	in std_logic;
+		MAX10_CLK1_50: in std_logic;
+		MAX10_CLK2_50: in std_logic;
+		
+		----------- SDRAM ------------
+		DRAM_ADDR: out std_logic_vector (12 downto 0);
+		DRAM_BA: out std_logic_vector (1 downto 0);
+		DRAM_CAS_N: out std_logic;
+		DRAM_CKE: out std_logic;
+		DRAM_CLK: out std_logic;
+		DRAM_CS_N: out std_logic;		
+		DRAM_DQ: inout std_logic_vector(15 downto 0);
+		DRAM_LDQM: out std_logic;
+		DRAM_RAS_N: out std_logic;
+		DRAM_UDQM: out std_logic;
+		DRAM_WE_N: out std_logic;
+		
+		----------- SEG7 ------------
+		HEX0: out std_logic_vector(7 downto 0);
+		HEX1: out std_logic_vector(7 downto 0);
+		HEX2: out std_logic_vector(7 downto 0);
+		HEX3: out std_logic_vector(7 downto 0);
+		HEX4: out std_logic_vector(7 downto 0);
+		HEX5: out std_logic_vector(7 downto 0);
+
+		----------- KEY ------------
+		KEY: in std_logic_vector(1 downto 0);
+
+		----------- LED ------------
+		LEDR: out std_logic_vector(9 downto 0);
+
+		----------- SW ------------
+		SW: in std_logic_vector(9 downto 0);
+
+		----------- VGA ------------
+		VGA_B: out std_logic_vector(3 downto 0);
+		VGA_G: out std_logic_vector(3 downto 0);
+		VGA_HS: out std_logic;
+		VGA_R: out std_logic_vector(3 downto 0);
+		VGA_VS: out std_logic;
+	
+		----------- Accelerometer ------------
+		GSENSOR_CS_N: out std_logic;
+		GSENSOR_INT: in std_logic_vector(2 downto 1);
+		GSENSOR_SCLK: out std_logic;
+		GSENSOR_SDI: inout std_logic;
+		GSENSOR_SDO: inout std_logic;
+	
+		----------- Arduino ------------
+		ARDUINO_IO: inout std_logic_vector(15 downto 0);
+		ARDUINO_RESET_N: inout std_logic
+	);
+end entity;
+
+
+architecture rtl of de10_lite is
+	signal clk : std_logic;
+	signal clear : std_logic;
+	signal w_flag : std_logic;
+	signal data_in : std_logic_vector (15 downto 0);
+	signal address : unsigned(1 downto 0);
+	signal data_out : std_logic_vector (15 downto 0);
+begin
+	
+	clk <= MAX10_CLK1_50;
+	clear <= SW(0);
+	w_flag <= SW(1);
+	data_in <= ARDUINO_IO;
+	LEDR <= data_out(9 downto 0);
+	
+	address <= unsigned(KEY);
+		
+	my_register_bank: entity work.registers
+		port map(
+			clk      => clk,
+			clear    => clear,
+			w_flag   => w_flag,
+			data_in  => data_in,
+			address  => address,
+			data_out => data_out
+		);
+	
+	
+	
+	
+
+end;
+
